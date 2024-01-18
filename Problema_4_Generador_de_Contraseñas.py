0719244320151b9f0aab69fb471156087bc2e1ff
Its.alexito__dev <110727609+itsalexitodev@users.noreply.github.com>
2024-01-18 18:17:49 +0100

    Actualizar Problema_4_Generador_de_Contraseñas.py

--- Problema_4_Generador_de_Contraseñas.py
+++ Problema_4_Generador_de_Contraseñas.py
@@ -3,7 +3,7 @@ Escribe un programa que genere una contrasenya aleatoria.
 La contrasenya debe tener una longitud predeterminada y contener letras mayusculas, letras minusculas y numeros.
 """
 
-import random
+
 
 characters = ["A","B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",
               "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
