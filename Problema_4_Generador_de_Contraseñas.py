1aeae5cbb431c3feb2ce4b25ac87ee84e37af28f
Alex Garcia Rodriguez <alexgaro2015@gmai.com>
2024-01-18 17:35:16 +0100

    areglando codigo

--- Problema_4_Generador_de_Contraseñas.py
+++ Problema_4_Generador_de_Contraseñas.py
@@ -5,12 +5,12 @@ La contrasenya debe tener una longitud predeterminada y contener letras mayuscul
 
 import random
 
-caracteres = ["A","B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",
+characters = ["A","B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",
               "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
               "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-for char in caracteres:
+for char in characters:
     for i in range(9):
         longitud = 8
-        contrasena = ''.join(random.choices(caracteres, k=longitud))
+        contrasena = ''.join(random.choices(characters, k=longitud))
         print(contrasena)
