67cbd09fee550f7a38a797d4c73ff9c383387fe6
Alex <alexgaro2015@gmail.com>
2024-01-24 09:27:31 +0100

    Problema 7 Resuelto

---
+++ Problema_7_Ordenar_Lista_de_Números.py
@@ -0,0 +1,15 @@
+"""""
+Escribe un programa que tome una lista de números como entrada 
+y devuelva la lista ordenada de forma ascendente.
+"""
+
+print("Ingrese una lista de números separados por espacio:")
+num_list =  [int(x) for x in input().split()]
+
+def ordenar_lista(lista):
+    lista_ordenada  = sorted(lista)
+    return lista_ordenada
+
+
+lista_ordenada = ordenar_lista(num_list)
+print(f"Lista ordenada: {lista_ordenada}")
