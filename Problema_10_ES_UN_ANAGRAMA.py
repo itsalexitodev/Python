3c1511bde8bc49dacb081171dbf0cd8f87f48ed5
Alex <alexgaro2015@gmail.com>
2024-01-26 11:50:04 +0100

    Problema 10 resuelto

--- Problema_10_ES_UN_ANAGRAMA.py
+++ Problema_10_ES_UN_ANAGRAMA.py
@@ -8,3 +8,21 @@
  * - Dos palabras exactamente iguales no son anagrama.
  */
  """
+
+print("Ingresa la primera palabra: ")
+palabra_usuario1 = input()
+
+print("Ingresa la segunda palabra: ")
+palabra_usuario2 = input()
+
+def verificar_anagrama(palabra1,palabra2 ):
+   palabra1 = palabra1.replace("","").lower()
+   palabra2 = palabra2.replace("","").lower()
+   return sorted(palabra1) == sorted(palabra2)
+
+resultado = verificar_anagrama(palabra_usuario1,palabra_usuario2)
+
+if resultado:
+    print(f"{palabra_usuario1} y {palabra_usuario2} son anagramas.")
+else:
+    print(f"{palabra_usuario1} y {palabra_usuario2} no son anagramas.")
