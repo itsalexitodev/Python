a2a3f2302cb8960233e7f539cf1688b5a874dbef
Alex <alexgaro2015@gmail.com>
2024-01-31 17:35:53 +0100

    new problem

---
+++ Problema_11_Cajero_automatico.py
@@ -0,0 +1,10 @@
+"""
+/*
+ * Haz un Progama que te reparta una canitdad dada X de dinero
+ * te imprima la cantidad de billetes necesarios
+ */
+ """
+
+print("Introduce la canitdad deseada a retirar")
+retirada = int(input())
+
