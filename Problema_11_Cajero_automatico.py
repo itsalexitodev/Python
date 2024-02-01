cafc7a0b4959db02f3a698b98a6942cfd96c87ad
Alex <alexgaro2015@gmail.com>
2024-02-01 16:02:43 +0100

    cajero

--- Problema_11_Cajero_automatico.py
+++ Problema_11_Cajero_automatico.py
@@ -5,6 +5,19 @@
  */
  """
 
-print("Introduce la canitdad deseada a retirar")
-retirada = int(input())
+def repartir_dinero(cantidad):
+    billetes = [50, 20, 10, 5, 2, 1]  
+    cantidades_billetes = {}  
+
+    for billete in billetes:
+        if cantidad >= billete:
+            cant_billetes = cantidad // billete
+            cantidad -= cant_billetes * billete
+            cantidades_billetes[billete] = cant_billetes
+
+    for billete, cantidad in cantidades_billetes.items():
+        print(f"Billetes de {billete}:", cantidad)
+
+cantidad_dinero = int(input("Ingrese la cantidad de dinero a repartir: "))
+repartir_dinero(cantidad_dinero)
 
