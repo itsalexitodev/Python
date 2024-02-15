7d54dc029eb7559eeff37232d8668c1be8d5a8da
Alex Garcia Rodriguez <alexgaro2015@gmai.com>
2024-02-15 17:15:43 +0100

    agenda

---
+++ agenda_contactos.py
@@ -0,0 +1,19 @@
+"""
+/*
+ * Es demana que desenvolupeu EN PARELLES una aplicació d’agenda de contactes en Python amb les
+ * següents funcionalitats:
+ * 1. Afegir contacte: Permet als usuaris afegir un nou contacte amb nom i número de telèfon.
+ * 2. Veure contactes: Mostra tots els contactes guardats.
+ * 3. Eliminar contacte: Permet als usuaris eliminar un contacte pel seu nom.
+ * 4. Cerca de contactes: Permet als usuaris cercar un contacte pel seu nom.
+ * 5. Guardar i carregar contactes en un fitxer: L’aplicació ha de permetre als usuaris desar els contactes en un
+ * fitxer i carregar-los posteriorment.
+ 
+ * afegir_contacte(nom, telèfon): Afegeix un contacte al diccionari.
+ * veure_contactes(): Mostra tots els contactes guardats.
+ * eliminar_contacte(nom): Elimina un contacte pel seu nom.
+ * cercar_contacte(nom): Cerca un contacte pel seu nom.
+ * desar_contactes(nom_fitxer): Guarda els contactes en un fitxer.
+ * carregar_contactes(nom_fitxer): Carrega els contactes des d’un fitxer.
+ */
+ """
